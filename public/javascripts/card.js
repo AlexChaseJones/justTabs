ec2375b0118257aff6fefd321ca6ca41fdc18104
Alexander <alexchasejones@icloud.com>
2016-06-10 13:47:04 -0400

    put link directly in HTML instead of handling it in JS

--- public/javascripts/card.js
+++ public/javascripts/card.js
@@ -22,10 +22,10 @@ $(document).ready(function(){
 	})
 });
 
-$(document).on('click', '.card-container', function(){
-	link = $(this).data('id');
-	$('#body-container').fadeOut(500,function(){
-		window.location.href = "tab/" + link;
-	})
-})
+// $(document).on('click', '.card-container', function(){
+// 	link = $(this).data('id');
+// 	$('#body-container').fadeOut(500,function(){
+// 		window.location.href = "tab/" + link;
+// 	})
+// })
 
